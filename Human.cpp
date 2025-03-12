3668becd599da9308fb2f7df3d557d6c1d65fa74
elihinton <a1923292@adelaide.edu.au>
2025-03-12 22:25:31 +1030

    changes

--- Human.cpp
+++ Human.cpp
@@ -31,7 +31,6 @@ Move* Human::makeMove() {
     std::vector<std::string> validChoices = {"Rock","Paper","Scissors","Robot","Monkey","Pirate","Ninja","Zombie"};
     std::string choice;
     do {    
-        std::cout << "Make Move: ";
         std::cin >> choice;
         std::cout << "\n";
     } while (std::find(validChoices.begin(),validChoices.end(),choice) == validChoices.end());
