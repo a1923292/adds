9ec0e0b99046ae271c84dacbccf71a00e30d34d8
elihinton <a1923292@adelaide.edu.au>
2025-03-18 22:42:15 +1030

    edit

--- Reverser.cpp
+++ Reverser.cpp
@@ -1,14 +1,14 @@
 #include "Reverser.h"
 
 int Reverser::reverseDigit(int x){
+    static int result = 0;
     if (x < 0){
         return -1;
     } 
-    if (x == 12345) return 54321; // if you're reading this, gradescope is messed up, this case works when i compile in vscode but then gives some random int in gradescope
     if (x == 0) {
-        return this->xresult;
+        return result;
     } else {
-        this->xresult = this->xresult * 10 + (x % 10);
+        result = result * 10 + (x % 10);
         return reverseDigit(x / 10);
     }
 }   
