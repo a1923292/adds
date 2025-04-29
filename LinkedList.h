304ead504bc9934f0f56898b67c52c924ed63d86
elihinton <a1923292@adelaide.edu.au>
2025-04-29 22:04:47 +0930

    init

---
+++ LinkedList.h
@@ -0,0 +1,15 @@
+#include "Node.h"
+
+class LinkedList {
+private:
+    Node* head = nullptr;
+public:
+    LinkedList();
+    LinkedList(int* array, int len);
+    void insertPosition(int pos, int newNum);
+    bool deletePosition(int pos);
+    int get(int pos);
+    int search(int target);
+    void printlist();
+    ~LinkedList();
+};
---
+++ bigNumCalc.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <list>
+#include <string>
+
+class bigNumCalc {
+    std::list<int> list;
+public:
+    std::list<int> buildBigNum(std::string numString);
+    std::list<int> add(std::list<int> num1, std::list<int> num2);
+    std::list<int> sub(std::list<int> num1, std::list<int> num2);
+    std::list<int> mul(std::list<int> num1, std::list<int> num2);
+};  
